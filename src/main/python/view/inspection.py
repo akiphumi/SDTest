b6a3cb81d693917b45dd8a937d0d937b08921b0b
akiphumi <akifumi.ichiki@gmail.com>
2020-11-15 11:19:23 +0800

    Update: serving_dobot to connect to the kv

--- src/main/python/view/inspection.py
+++ src/main/python/view/inspection.py
@@ -2,6 +2,7 @@ from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from model.camera_model import CameraModel
 from model.project import Project
 from model.learning_model import LearningModel
+from model.serving_dobot import ServingDobot
 from view.ui.inspection import Ui_inspection
 from view.camera_list import CameraList
 from PyQt5.QtWidgets import QWidget, QMessageBox, QFileDialog
@@ -17,6 +18,7 @@ class InspectionWidget(QWidget):
 
     # this signal emit on shortage of ai models
     model_file_not_found_error = pyqtSignal()
+    inspection_finished = pyqtSignal(bool)
     __INSPECTED_IMAGES_DIR_NAME = '/inspection_results/images'
     __VIEW_FINDER = QSize(400, 225)
 
@@ -30,6 +32,10 @@ class InspectionWidget(QWidget):
         self.camera_model.image_saved.connect(self.on_image_saved)
 
         self.ui.camera_preview.setFixedSize(self.__VIEW_FINDER)
+
+        self.serving_dobot = ServingDobot()
+        self.serving_dobot.dobot_req.connect(self.on_clicked_inspect_button)
+        self.inspection_finished.connect(self.serving_dobot.sending_inspection_result)
 
         self.learning_model = LearningModel.default()
         self.learning_model.predicting_finished.connect(self.on_finished_predicting)
@@ -110,6 +116,7 @@ class InspectionWidget(QWidget):
         score = result['scores']
         prediction = result['prediction']
         self.ui.loader_label.clear()
+        self.inspection_finished.emit(prediction == 'good')
         if prediction == 'good':
             self.ui.result.setCurrentWidget(self.ui.OK)
             move(image_path, inspected_image_dir_path + '/OK_' + image_name)
